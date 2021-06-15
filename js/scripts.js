614abe2792ecd29912c83025ef8639c593228c0c
alexseymourdev <alexseymourdev@gmail.com>
2021-06-15 20:12:10 +0100

    adding code to get and format current time

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log('test');
+// get the current date object
+var today = new Date();
+//get the current hour
+let hours = today.getHours();
+//get the current minutes
+let minutes = today.getMinutes();
+//if the length of the minuts is only 1 add a 0 first (7 = 07)
+if(minutes.length == 1){
+    minutes = '0' + minutes;
+}
+//get the current seconds
+let seconds = today.getSeconds();
+//set the current time as hours, minutes and seconds
+let time = hours + ":" + minutes + ":" + seconds;
+console.log(time);
