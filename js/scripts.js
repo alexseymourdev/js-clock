dd37f1feeaae47e69d744b4c5e2fedfa4bdd4432
alexseymourdev <alexseymourdev@gmail.com>
2021-06-15 20:39:13 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,28 @@
-// get the current date object
-var today = new Date();
-//get the current hour
-let hours = today.getHours();
-//get the current minutes
-let minutes = today.getMinutes();
-//if the length of the minuts is only 1 add a 0 first (7 = 07)
-if(minutes.length == 1){
-    minutes = '0' + minutes;
+function outputTime() {
+
+    setTimeout(function() {
+        // get the current date object
+        var today = new Date();
+        //get the current hour
+        let hours = today.getHours();
+        //get the current minutes
+        let minutes = today.getMinutes();
+        //get the current seconds
+        let seconds = today.getSeconds();
+        //set the current time as hours, minutes and seconds
+        let time = hours + ":" + minutes + ":" + seconds;
+        // console.log(time);
+
+        //get clock output HTML object
+        let output = document.querySelector('.clockOutput');
+        // console.log(output);
+        output.innerHTML = time;
+
+        // Again
+        outputTime();
+        // Every 250 miliseconds 
+    }, 250);
 }
-//get the current seconds
-let seconds = today.getSeconds();
-if(seconds.length == 1){
-    seconds = '0' + seconds;
-}
-//set the current time as hours, minutes and seconds
-let time = hours + ":" + minutes + ":" + seconds;
-console.log(time);
+
+// Begins
+outputTime();
